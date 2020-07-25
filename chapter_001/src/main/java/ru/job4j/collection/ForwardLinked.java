bb080a0cb406f24da62dea17758b255d6b973c4e
Ekaterina Kalashnikova <ladykati73@gmail.com>
2020-07-25 15:42:04 +0300

    Удалить head в односвязном списке. [#301064]

--- chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -1,7 +1,5 @@
 package ru.job4j.collection;
 
-import com.sun.jdi.Value;
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -26,35 +24,21 @@ public class ForwardLinked<T> implements Iterable<T> {
      * Метод удаления элемента из списка, когда проверяем
      * что список состоит только из одного элемента,
      * из двух: головы и хвоста, если тот который нам нужен это голова,
-     * перключаемся на второй,
-     * или начинаем искать и если он последний, то переключаем на текущий,
-     * или ищем дальше.
+     * перключаемся на второй, следующий.
+     * @return value - значение оставшегося элемента
      */
-    public  void deleteFirst() {
+    public T deleteFirst() {
         if (head == null) {
-            return;
+            throw new NoSuchElementException();
         }
         if (head == tail) {
             head = null;
             tail = null;
-            return;
+            return null;
         }
-        Node<T> value = head.next;
-        if (head.value == value) {
-            head = head.next;
-            return;
-        }
-        Node<T> element = head;
-        while (element.next != null) {
-            if (element.next.value == value) {
-                if (element.next == tail) {
-                    element = tail;
-                }
-                element.next = element.next.next;
-                return;
-            }
-            element = element.next;
-        }
+        T value = (T) head.next;
+         head = head.next;
+         return value;
     }
 
     @Override
