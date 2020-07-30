3794847cb4cf9103b5015da57c420a9663beaa06
Ekaterina Kalashnikova <ladykati73@gmail.com>
2020-07-30 21:28:46 +0300

    Создать модель User [#301094]

---
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,51 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+public class User {
+
+    /**
+     * Поле имени пользователя.
+     * Поле количества детей.
+     * Поле дня рождения
+     */
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * Конструктор инициализации класса по трем полям
+     * @param name - имя пользователя
+     * @param children - количество детей
+     * @param birthday - дни рождения
+     */
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
