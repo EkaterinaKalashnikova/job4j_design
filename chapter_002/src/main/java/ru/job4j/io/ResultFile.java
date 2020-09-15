bed75b3afcc7f518525628b6ff4ec6512f1fc60c
Ekaterina Kalashnikova <ladykati73@gmail.com>
2020-09-15 20:05:05 +0300

    FileOutputStream. [#301114]

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -8,7 +8,7 @@ public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i < 10; i++) {
-                for (int j = 2; j < 10; j++) {
+                for (int j = 1; j < 10; j++) {
                     String s = j + "*" + i + "=" + (i * j)  + "\t\t";
                     System.out.println(s);
                 }
