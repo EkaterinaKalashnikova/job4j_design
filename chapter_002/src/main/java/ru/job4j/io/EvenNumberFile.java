17bb0dae2a689d68f06a6e173576c992026f0508
Ekaterina Kalashnikova <ladykati73@gmail.com>
2020-09-16 21:09:18 +0300

    FileInputStream [#301123]

--- chapter_002/src/main/java/ru/job4j/io/EvenNumberFile.java
+++ chapter_002/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -6,15 +6,21 @@ import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-       try (FileInputStream in = new FileInputStream("even.txt")) {
+       try (FileInputStream in = new FileInputStream("C:\\projects.even.txt")) {
            StringBuilder text = new StringBuilder();
            int read;
            while ((read = in.read()) != -1) {
                text.append((char) read);
            }
            int num = 10;
-           boolean rsl = num % 2 == 0;
-           System.out.println(rsl);
+           String[] lines = text.toString().split(System.lineSeparator());
+           for (String line : lines) {
+               int lineInt = Integer.parseInt("line");
+               if (lineInt % 2 == 0) {
+                   boolean rsl = true;
+               }
+               System.out.println(lineInt);
+           }
        } catch (IOException e) {
            e.printStackTrace();
        }
--- chapter_002/src/main/java/ru/job4j/io/ReadFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ReadFile.java
@@ -4,23 +4,17 @@ import java.io.FileInputStream;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("C:\\projects.even.txt")) {
+        try (FileInputStream in = new FileInputStream("input.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
-           // System.out.println(text);
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                int lineInt = Integer.parseInt("line");
-                if (lineInt % 2 == 0) {
-                   boolean rsl = true;
-                }
-                System.out.println(lineInt);
-            }
+            System.out.println(text);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
