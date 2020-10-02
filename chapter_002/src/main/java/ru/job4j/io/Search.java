e6e7f4367ebcf88870954e70123dcb943fcdc07f
Ekaterina Kalashnikova <ladykati73@gmail.com>
2020-10-02 22:03:48 +0300

    4.1. Сканирование файловой системы. [#301111]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -1,21 +1,32 @@
 package ru.job4j.io;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Search {
+
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
         search(start, "js").forEach(System.out::println);
     }
 
-    public static List<Path> search(Path root, String ext) {
-       // SearchFiles searcher = new SearchFiles(p -> p.toFile().getName.endsWith(ext));
-       // Files.walkFileTree(root, searcher);
-      //  return searcher.getPaths();
-        return null;
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName.endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static class MyFileVisitor extends SimpleFileVisitor<Path> {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+           List<String> stringList = Files.lines(Path.of("."))
+                   .filter(line -> line.contains("js"))
+                   .collect(Collectors.toList());
+           return FileVisitResult.CONTINUE;
+        }
     }
 }
--- chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
+++ chapter_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -1,13 +1,20 @@
 package ru.job4j.io;
 
 import java.io.IOException;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
-public class SearchFiles {
+public abstract class SearchFiles implements FileVisitor<Path> {
+
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
         Files.walkFileTree(start, new PrintFiles());
+    }
+
+    public List<Path> getPaths() {
+        return null;
     }
 }
