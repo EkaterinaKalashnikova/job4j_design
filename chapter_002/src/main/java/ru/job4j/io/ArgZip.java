a45afce4eaf11266c70ad6bae67ffcc7655484b6
Ekaterina Kalashnikova <ladykati73@gmail.com>
2020-10-18 21:50:56 +0300

    Архивировать проект [#301121]

--- chapter_002/src/main/java/ru/job4j/io/ArgZip.java
+++ chapter_002/src/main/java/ru/job4j/io/ArgZip.java
@@ -7,7 +7,6 @@ public class ArgZip {
     private final String[] args;
     private final static int ARG = 3;
     private final static String DIRECTORY = "C:\\projects\\job4j_design\\";
-   // private final static String DIRECTORY = "C:\\projects\\job4j\\";
     private final static String EXCLUDE = ".xml";
     private final static String OUTPUT = "Job4j.zip";
 
@@ -19,7 +18,7 @@ public class ArgZip {
         if (args.length != ARG) {
            throw new IllegalArgumentException("Invalid args: java -jar pack.jar -d=c:\\project\\job4j\\ -e=class -o=project.zip");
         }
-        File file = new File(args[ Integer.parseInt(DIRECTORY + 1) ]);
+        File file = new File(args[ Integer.parseInt(DIRECTORY) ]);
         if (!file.isDirectory()) {
             throw  new IllegalArgumentException("Invalid catalog");
         }
@@ -30,14 +29,14 @@ public class ArgZip {
     }
 
     public String directory() {
-        return args[ Integer.parseInt(DIRECTORY + 1) ];
+        return args[ Integer.parseInt(DIRECTORY) ];
     }
 
     public String exclude() {
-        return args[ Integer.parseInt(EXCLUDE + 1) ];
+        return args[ Integer.parseInt(EXCLUDE) ];
     }
 
     public String output() {
-        return args[ Integer.parseInt(OUTPUT + 1) ];
+        return args[ Integer.parseInt(OUTPUT) ];
     }
 }
--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -1,19 +1,33 @@
 package ru.job4j.io;
 
+import javax.sound.midi.Patch;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public static List<Path> search(Path root, String ext) throws IOException {
-        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
-        Files.walkFileTree(root, searcher);
-        return searcher.getPaths();
-    }
 
+    /**
+     * Метод поиска поиск файлов типа ArgZip, исключая заданное расширение
+     */
+    public static List<Path> search(ArgZip argZip) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(argZip.exclude()));
+        Files.walkFileTree(Paths.get(argZip.directory()), searcher);
+        return searcher.getPaths();
+     }
+
+    /**
+     *
+     * @param sources
+     * @param target
+     */
     public void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File source : sources) {
@@ -27,6 +41,11 @@ public class Zip {
         }
     }
 
+    /**
+     *
+     * @param source
+     * @param target
+     */
     public void packSingleFile(File source, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             zip.putNextEntry(new ZipEntry(source.getPath()));
@@ -38,13 +57,18 @@ public class Zip {
         }
     }
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        ArgZip argZip = new ArgZip(args);
+        List<Path> filesToArchive = new ArrayList<>();
+        Zip zip = new Zip();
+        filesToArchive.forEach(f -> filesToArchive.stream().map(Path::getFileName).collect(Collectors.toList()));
         new Zip().packSingleFile(
                 new File("./chapter_005/pom.xml"),
                 new File("./chapter_005/pom.zip")
-               // new File("./chapter_006/pom.xml"),
-               // new File("./chapter_006/pom.zip")
         );
     }
 }
-git
