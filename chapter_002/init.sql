28b8477a1c56ecaa4d2f0b661f12b1ecf9f50708
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-02-17 17:00:56 +0300

    Схема прав пользователи и роли. [#435976]

--- chapter_002/init.sql
+++ chapter_002/init.sql
@@ -40,3 +40,40 @@ create table attachs(
 	id serial primary key,
 	scroll varchar(2000)
 );
+
+create table users_roles(
+	id serial primary key,
+	users_id int references users(id)
+);
+
+create table roles_rules(
+	id serial primary key,
+	roles_id int references users(id),
+	rules_id int references rules(id)
+);
+
+create table item_users(
+	id serial primary key,
+	item_id int references item(id)
+);
+
+create table item_commentses(
+	id serial primary key,
+	commentses_id int references commentses(id)
+);
+
+create table item_attachs(
+	id serial primary key,
+	attachs_id int references attachs(id)
+);
+
+create table item_category(
+	id serial primary key,
+	item_id int references item(id)
+);
+
+create table item_state(
+	id serial primary key,
+	item_id int references item(id)
+);
+
