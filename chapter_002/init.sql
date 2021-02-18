58fec6bda86b22e37b6bf24fa3e8088034d5a641
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-02-18 18:00:12 +0300

    Схема прав пользователи и роли. [#435976]

--- chapter_002/init.sql
+++ chapter_002/init.sql
@@ -1,79 +1,53 @@
-create table users (
-	id serial primary key,
-	name varchar(255)
-);
-
 create table roles(
 	id serial primary key,
 	name varchar(2000),
-	qualification_id text
+	qualification text
+);
+
+create table users (
+	id serial primary key,
+	name varchar(255),
+	roles_id int references roles(id)
 );
 
 create table rules(
 	id serial primary key,
-	action_id varchar(2000),
-	user_id varchar(255)
+	action varchar(2000)
+);
+
+create table states(
+	id serial primary key,
+	realisation text
+);
+
+create table category(
+	id serial primary key,
+	kind text
 );
 
 create table item(
 	id serial primary key,
 	name varchar(255),
-	description_id text
-);
-
-create table category(
-	id serial primary key,
-	kind_id text
-);
-
-create table states(
-	id serial primary key,
-	realisation_id text
+	description text,
+	users_id int references users(id),
+	category_id int references category(id),
+	states_id int references states(id)
 );
 
 create table commentses(
 	id serial primary key,
-	feature varchar(2000)
+	feature varchar(2000),
+	item_id int references item(id)
 );
 
 create table attachs(
 	id serial primary key,
-	scroll varchar(2000)
-);
-
-create table users_roles(
-	id serial primary key,
-	users_id int references users(id)
+	scroll varchar(2000),
+	item_id int references item(id)
 );
 
 create table roles_rules(
 	id serial primary key,
-	roles_id int references users(id),
+	roles_id int references roles(id),
 	rules_id int references rules(id)
 );
-
-create table item_users(
-	id serial primary key,
-	item_id int references item(id)
-);
-
-create table item_commentses(
-	id serial primary key,
-	commentses_id int references commentses(id)
-);
-
-create table item_attachs(
-	id serial primary key,
-	attachs_id int references attachs(id)
-);
-
-create table item_category(
-	id serial primary key,
-	item_id int references item(id)
-);
-
-create table item_state(
-	id serial primary key,
-	item_id int references item(id)
-);
-
--- chapter_002/insert.sql
+++ chapter_002/insert.sql
@@ -1,61 +1,38 @@
-insert into users(name) values('Поляков Артем');
-insert into users(name) values('Рудный Виталий');
-insert into users(name) values('Мироненко Александр');
+insert into roles(name, qualification)values('сантехник', '1разряд');
+insert into roles(name, qualification)values('сантехник', '2разряд');
+insert into roles(name, qualification)values('сантехник', '3разряд');
 
-insert into roles(name, qualification_id)values('сантехник', '1разряд');
-insert into roles(name, qualification_id)values('сантехник', '2разряд');
-insert into roles(name, qualification_id)values('сантехник', '3разряд');
+insert into users(name, roles_id) values('Поляков Артем', '32');
+insert into users(name, roles_id) values('Рудный Виталий', '32');
+insert into users(name, roles_id) values('Мироненко Александр', '33');
 
-insert into rules(action_id)values('допуск в ТУ');
-insert into rules(action_id)values('допуск к оборудованию');
-insert into rules(action_id)values('допуск в ЦТП');
+insert into rules(action)values('допуск в ТУ');
+insert into rules(action)values('допуск к оборудованию');
+insert into rules(action)values('допуск в ЦТП');
 
-insert into item(name, description_id)values('течь трубопровода', 'по трубе хвс течь из-под вентиля');
-insert into item(name, description_id)values('течет радиатор', 'капает со стыка трубы отопления и радиатора');
-insert into item(name, description_id)values('вода под домом', 'из ТУ течет канализация');
+insert into states(realisation)values('в процессе');
+insert into states(realisation)values('принята');
+insert into states(realisation)values('выполнено');
 
-insert into category(kind_id)values('срочная');
-insert into category(kind_id)values('текущая');
-insert into category(kind_id)values('профилактика');
+insert into category(kind)values('срочная');
+insert into category(kind)values('текущая');
+insert into category(kind)values('профилактика');
 
-insert into states(realisation_id)values('в процессе');
-insert into states(realisation_id)values('принята');
-insert into states(realisation_id)values('выполнено');
+insert into item(name, description, users_id, category_id, states_id)values('течь трубопровода', 'по трубе хвс течь из-под вентиля', '12', '4', '4');
+insert into item(name, description, users_id, category_id, states_id)values('течет радиатор', 'капает со стыка трубы отопления и радиатора', '14', '5', '5');
+insert into item(name, description, users_id, category_id, states_id)values('вода под домом', 'из ТУ течет канализация', '13', '6', '6');
 
-insert into commentses(feature)values('ленинградская система отопления');
-insert into commentses(feature)values('недавно заменен трубопровод');
-insert into commentses(feature)values('металлопластиковые трубы');
+insert into commentses(feature, item_id)values('ленинградская система отопления', '12');
+insert into commentses(feature, item_id)values('недавно заменен трубопровод', '10');
+insert into commentses(feature, item_id)values('металлопластиковые трубы', '11');
 
-insert into attachs(scroll)values('схема');
-insert into attachs(scroll)values('снимок');
-insert into attachs(scroll)values('график проведенных работ');
+insert into attachs(scroll, item_id)values('схема', '12');
+insert into attachs(scroll, item_id)values('снимок', '11');
+insert into attachs(scroll, item_id)values('график проведенных работ', '10');
 
-insert into users_roles(users_id) values('1');
-insert into users_roles(users_id) values('2');
-insert into users_roles(users_id) values('3');
+insert into roles_rules(roles_id, rules_id) values('31', '4');
+insert into roles_rules(roles_id, rules_id) values('32', '5');
+insert into roles_rules(roles_id, rules_id) values('33', '6');
 
-insert into roles_rules(roles_id, rules_id) values('1', '3');
-insert into roles_rules(roles_id, rules_id) values('2', '1');
-insert into roles_rules(roles_id, rules_id) values('3', '2');
-
-insert into item_users(item_id) values('1');
-insert into item_users(item_id) values('2');
-insert into item_users(item_id) values('3');
-
-insert into item_commentses(commentses_id) values('1');
-insert into item_commentses(commentses_id) values('2');
-insert into item_commentses(commentses_id) values('3');
-
-insert into item_attachs(attachs_id) values('1');
-insert into item_attachs(attachs_id) values('2');
-insert into item_attachs(attachs_id) values('3');
-
-insert into item_category(item_id) values('1');
-insert into item_category(item_id) values('2');
-insert into item_category(item_id) values('3');
-
-insert into item_state(item_id) values('1');
-insert into item_state(item_id) values('2');
-insert into item_state(item_id) values('3');
 
 
