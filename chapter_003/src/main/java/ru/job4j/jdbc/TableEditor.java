f0e6e93fa4b45b9ac7209e8ffcbfa1ee6c1d9554
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-03-01 17:42:07 +0300

    0.1. Statement [#435952]

--- chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
+++ chapter_003/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -1,38 +1,67 @@
 package ru.job4j.jdbc;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class TableEditor implements AutoCloseable {
     private Connection connection;
-
     private Properties properties;
 
-    public TableEditor(Properties properties) {
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
         this.properties = properties;
         initConnection();
     }
 
-    private void initConnection() {
-        connection = null;
+    private void initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection(properties.getProperty("url"));
+        properties.getProperty("username");
+        properties.getProperty("password");
     }
 
+    //создание запросов
+    private void execute(String sql) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //создает пустую таблицу без столбцов с указанным именем
     public void createTable(String tableName) {
+        String sql = String.format(
+                "create table %s (%s, %s);",
+                tableName,
+                "id serial primary key",
+                "name varchar(255)"
+        );
+        execute(sql);
     }
 
+     //удаляет таблицу по указанному имени
     public void dropTable(String tableName) {
+        String sql = String.format("drop table %s", tableName);
+        execute(sql);
     }
 
+     //добавляет столбец в таблицу
     public void addColumn(String tableName, String columnName, String type) {
+        String sql = String.format("alter table %s add column %s, %s", tableName, columnName, type);
+        execute(sql);
     }
 
+      //удаляет столбец из таблицы
     public void dropColumn(String tableName, String columnName) {
+        String sql = String.format("alter table %s drop column %s", tableName, columnName);
+        execute(sql);
     }
 
+      //переименовывает столбец
     public void renameColumn(String tableName, String columnName, String newColumnName) {
+        String sql = String.format("alter table %s rename column %s, %s", tableName, columnName, newColumnName);
+        execute(sql);
     }
 
     public String getScheme(String tableName) throws SQLException {
