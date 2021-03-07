0c9e751f134e0072891898f1817fd20dcd11a930
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-03-07 13:30:42 +0300

    Sql схема для проекта Tracker. [#435951]

---
+++ chapter_003/src/main/java/ru/job4j/db/insert.sql
@@ -0,0 +1,5 @@
+insert into items(name) values('Оценка повреждений кузова');
+insert into items(name) values('Замена моторного масла');
+insert into items(name) values('Балансировка колес');
+insert into items(name) values('Ремонт глушителя');
+insert into items(name) values('Ремонт двигателя');
