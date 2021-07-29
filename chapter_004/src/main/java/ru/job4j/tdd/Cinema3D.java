a2ad00cfbc72147c1fafcb391e45a71e56a056a3
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-07-29 21:47:01 +0300

    Что такое TDD? [#4918]

--- chapter_004/src/main/java/ru/job4j/tdd/Cinema3D.java
+++ chapter_004/src/main/java/ru/job4j/tdd/Cinema3D.java
@@ -1,14 +1,16 @@
 package ru.job4j.tdd;
 
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Cinema3D implements Cinema {
 
+
     @Override
     public List<Session> find(Predicate<Session> filter) {
-        return null;
+        return Collections.unmodifiableList((List<Session>) new Session3D());
     }
 
     @Override
--- chapter_004/src/test/java/ru/job4j/tdd/CinemaTest.java
+++ chapter_004/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -20,6 +20,14 @@ public class CinemaTest {
     }
 
     @Test
+    public void findNot() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Test
     public void buy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
@@ -30,6 +38,15 @@ public class CinemaTest {
     }
 
     @Test
-    public void add() {
+    public void buyErr() {
+        Account account = new AccountCinema();
+        Account account1 = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket1 = cinema.buy(account1, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+        assertThat(ticket1, is(new Ticket3D()));
     }
 }
