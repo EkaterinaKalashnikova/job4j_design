3520a53f8a7b8dd49242c8399dd27332a07db52c
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-07-31 20:28:19 +0300

    Что такое TDD? [#4918]

--- chapter_004/src/test/java/ru/job4j/tdd/CinemaTest.java
+++ chapter_004/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 
 public class CinemaTest {
@@ -16,7 +17,7 @@ public class CinemaTest {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-        assertThat(sessions, is(Arrays.asList(new Session3D())));
+        assertThat(sessions, is(Collections.singletonList(new Session3D())));
     }
 
     @Test
@@ -38,15 +39,19 @@ public class CinemaTest {
     }
 
     @Test
-    public void buyErr() {
+    public void buyErr(int row, int column) {
         Account account = new AccountCinema();
         Account account1 = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
         date.set(2020, 10, 10, 23, 00);
-        Ticket ticket = cinema.buy(account, 1, 1, date);
-        Ticket ticket1 = cinema.buy(account1, 1, 1, date);
-        assertThat(ticket, is(new Ticket3D()));
-        assertThat(ticket1, is(new Ticket3D()));
+        if (row >= 1 & row <= 15) {
+            if (column >= 1 & column <= 20) {
+                Ticket ticket = cinema.buy(account, 1, 1, date);
+                Ticket ticket1 = cinema.buy(account1, 1, 1, date);
+                assertThat(ticket, is(new Ticket3D()));
+                assertThat(ticket1, is(new Ticket3D()));
+            }
+        }
     }
 }
