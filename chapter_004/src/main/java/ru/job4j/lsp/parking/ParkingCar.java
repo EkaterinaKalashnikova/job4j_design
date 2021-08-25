11e769254277aa394a99ec32c67e48661fc57508
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-08-26 00:15:27 +0300

    Парковка машин [#853 #189160]

--- chapter_004/src/main/java/ru/job4j/lsp/parking/ParkingCar.java
+++ chapter_004/src/main/java/ru/job4j/lsp/parking/ParkingCar.java
@@ -26,12 +26,12 @@ public class ParkingCar implements Parking {
         if (car.getSizeCar() == 1 && (countFreePlace - car.getSizeCar()) > 0) {
             cars.add(car);
             countFreePlace = countFreePlace - 1;
-        } else if (car.getSizeCar() > 1 && (countTruckPlace - 1) > 0) {
-            trucks.add(car);
-            countTruckPlace = countTruckPlace - 1;
         } else if (car.getSizeCar() > 1 && (countFreePlace - car.getSizeCar()) > 0) {
             trucks.add(car);
             countFreePlace = countFreePlace - car.getSizeCar();
+        } else if (car.getSizeCar() > 1 && (countTruckPlace - 1) > 0) {
+            trucks.add(car);
+            countTruckPlace = countTruckPlace - 1;
         } else {
             return false;
         }
