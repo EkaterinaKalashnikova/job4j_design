ea3f8df24d7417f8958b5c06bbcb8600babced1d
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-09-15 21:33:48 +0300

    Динамическое перераспределение продуктов   [#854]

--- src/test/java/ru/job4j/productsrefactoring/ControllQualityTest.java
+++ src/test/java/ru/job4j/productsrefactoring/ControllQualityTest.java
@@ -4,6 +4,9 @@ import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
@@ -98,13 +101,47 @@ class ControllQualityTest {
         controlQuality.distribution(food1);
         controlQuality.distribution(food2);
         Assert.assertEquals(warehouse.getFoods().get(0).getName(), "Cheese");
-        Assert.assertEquals(trash.getFoods().get(0).getName(),"Milk");
+        Assert.assertEquals(trash.getFoods().get(0).getName(), "Milk");
         Assert.assertEquals(shop.getFoods().get(0).getName(), "Bread");
         food.setExpiryDate(expireDate1);
         food1.setExpiryDate(expireDate2);
         food2.setExpiryDate(expireDate);
         controlQuality.resort();
         Assert.assertEquals(trash.getFoods().get(0).getName(), "Bread");
+        Assert.assertTrue(warehouse.getFoods().contains(food1));
+        Assert.assertTrue(shop.getFoods().contains(food2));
+    }
+
+    @Test
+    public void whenAddFromWarehouse2() {
+        Calendar expireDate = Calendar.getInstance();
+        Calendar expireDate1 = Calendar.getInstance();
+        Calendar expireDate2 = Calendar.getInstance();
+        expireDate.add(Calendar.DAY_OF_MONTH, 15);
+        expireDate1.add(Calendar.DAY_OF_MONTH, 0);
+        expireDate2.add(Calendar.DAY_OF_YEAR, 90);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar createDate = Calendar.getInstance();
+        dateFormat.format(createDate.getTime());
+        createDate.add(Calendar.DATE, -10);
+        Food food = new Food("Bread", expireDate, createDate, 27.0);
+        Food food1 = new Food("Milk", expireDate1, createDate, 54.0);
+        Food food2 = new Food("Cheese", expireDate2, createDate, 560.0);
+        Storage warehouse = new Warehouse();
+        Storage shop = new Shop();
+        Storage trash = new Trash();
+        ControlQuality controlQuality = new ControlQuality(List.of(warehouse, shop, trash));
+        controlQuality.distribution(food);
+        controlQuality.distribution(food1);
+        controlQuality.distribution(food2);
+        Assert.assertEquals(warehouse.getFoods().get(0).getName(), "Cheese");
+        Assert.assertEquals(trash.getFoods().get(0).getName(), "Milk");
+        Assert.assertEquals(shop.getFoods().get(0).getName(), "Bread");
+        food.setExpiryDate(expireDate1);
+        food1.setExpiryDate(expireDate2);
+        food2.setExpiryDate(expireDate);
+        controlQuality.resort();
+        Assert.assertTrue(trash.getFoods().contains(food));
         Assert.assertTrue(warehouse.getFoods().contains(food1));
         Assert.assertTrue(shop.getFoods().contains(food2));
     }
